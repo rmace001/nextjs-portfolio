b4f90ac1e4f64efb4f26f9b71f62e3f7b3b3b8be
Rogelio Macedo <rmace001@ucr.edu>
2025-01-19 18:36:37 -0600

    enh: add title

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
         <html lang="en" data-theme="lofi">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <title>Rogelio Macedo</title>
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
